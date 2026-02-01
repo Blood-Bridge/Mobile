b71c93cf9323747835cbcb2988ed1a7a40a8f3e9
Ahmed Khaled <AHMEDKHALED195@users.noreply.github.com>
2026-02-01 03:11:23 +0200

    add AppColors class with primary and button color constants

--- lib/const.dart
+++ lib/const.dart
@@ -1,2 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Color
+  static const Color primary = Color(0xFF0A0A0A);
+
+  // Button Color
+  static const Color button = Color(0xFFC97777);
+}
