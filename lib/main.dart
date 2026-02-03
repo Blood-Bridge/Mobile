c49e8a3e4ef75ba2103402daa51de44a161f2328
Ahmed Khaled <AHMEDKHALED195@users.noreply.github.com>
2026-02-03 02:37:56 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:blood_bridge/core/services/hive_helper.dart';
 import 'package:blood_bridge/core/utiles/app_colors.dart';
-import 'package:blood_bridge/features/on_boarding/presentaion/views/on_boarder_view.dart';
 import 'package:blood_bridge/features/splash/presentaion/views/splash_view.dart';
-import 'package:blood_bridge/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  Hive.openBox(HiveHelper.onboardingBox);
+  await Hive.openBox(HiveHelper.onboardingBox);
   runApp(const MyApp());
 }
 
